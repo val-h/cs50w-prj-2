c5b1a6c7504528a83f259837b7aa85d30e04ce9c
Valentin Mirchev <valentin97mirchev@gmail.com>
2021-02-20 23:54:20 +0000

    started listings

--- auctions/urls.py
+++ auctions/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "auctions"
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+
+    # my paths
+    path("create-listing/", views.create_listing, name="create_listing"),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+# My views
+def create_listing(request):
+    pass
