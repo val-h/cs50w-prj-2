cc33dc5edf54f37f686beb20b2f873631e735467
Valentin Mirchev <valentin97mirchev@gmail.com>
2021-03-21 12:42:47 +0000

    collected static f iles, fixed the logo image and moved it to static

--- commerce/settings.py
+++ commerce/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'auctions/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = '/home/val-h/Dev/web50-projects/commerce/media'
